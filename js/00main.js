34cd1968d33cd2a3ac461de764d747f25cb77e33
Daniela <danisalazarcueto7@gmail.com>
2022-04-18 20:05:38 +0100

    interactive part done

---
+++ js/00main.js
@@ -0,0 +1,51 @@
+/* to do: al deslizar el 'slider' se tiene que ir incrementando el precio de diferentes numeros de vista de página. 
+Si el visitante cambia el 'toggle' para facturación anual, se aplicará un 25% de descuento en todos los precios.  */
+
+/*
+- 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
+*/
+
+"use strict";
+
+let views = document.querySelector(".card__views"); //
+let slider = document.querySelector(".slider");
+let priceMonth = document.querySelector(".price__container--pricing");
+let switchToggle = document.querySelector(".switch");
+
+//console.log(views, slider, priceMonth, switchToggle);
+
+const viewData = [
+  {
+    pageViews: "10K",
+    monthlyCost: 8,
+  },
+  {
+    pageViews: "50K",
+    monthlyCost: 12,
+  },
+  {
+    pageViews: "100K",
+    monthlyCost: 16,
+  },
+  {
+    pageViews: "500K",
+    monthlyCost: 24,
+  },
+  {
+    pageViews: "1M",
+    monthlyCost: 36,
+  },
+];
+
+function updatePricing() {
+  const packetId = parseInt(slider.value);
+  const packetSelected = viewData[packetId];
+  console.log(packetSelected);
+  views.innerHTML = `${packetSelected.pageViews} PAGEVIEWS`;
+  priceMonth.innerHTML = `$${packetSelected.monthlyCost.toFixed(2)}`;
+}
+slider.addEventListener("change", updatePricing);
